8272d220cc327ce40be2a585f26f08fee78c78a4
Nina Jade <jaderiver64@gmail.com>
2021-01-14 17:36:20 -0600

    Displays current date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 var events = {};
-var createEvent = function(eventText, eventDate, eventTime) {}
+
+
+var currentMomentObj = moment();
+var currentMomentString = currentMomentObj.format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+var createCalendar = function(thisMoment) {
+
+    var timeArray = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
+
+    $("#currentDay").append(currentMomentString);
+
+};
+createCalendar();
