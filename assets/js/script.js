6fbba710d68ce3d048245f7f961ebff6048f60fa
Nina Jade <jaderiver64@gmail.com>
2021-01-14 18:05:03 -0600

    dynamically builds the skeleton of the scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,23 @@ var createCalendar = function(thisMoment) {
     var timeArray = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
 
     $("#currentDay").append(currentMomentString);
+    var amPm = "am";
+    for (var i = 0; i < timeArray.length; i++) {
+        var timeEl = $("<div>")
+            .addClass("time-element")
+            .text(timeArray[i] + amPm);
+        if (timeArray[i] === 11) { amPm = "pm"; };
+        var eventEl = $("<div>")
+            .addClass("event-element")
+            .text("Event Info");
+        var saveIconEl = $("<div>")
+            .addClass("save-icon-element")
+            .text("save icon");
+        $("#time-column").append(timeEl);
+        $("#event-column").append(eventEl);
+        $("#save-icon").append(saveIconEl);
+
+    }
 
 };
 createCalendar();
