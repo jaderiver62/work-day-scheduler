897fb5aa20e3676d90170cd919fe2387c17c57fe
Nina Jade <jaderiver64@gmail.com>
2021-01-18 02:35:13 -0600

    changed interval to 30 mins

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
+// Initializing the array which holds the event objects
 var events = [];
+// Create a moment object for the current date and time
 var currentMomentObj = moment();
 var currentMomentString = currentMomentObj.format("dddd, MMMM Do YYYY, h:mm:ss a");
+// Formatting the object into a string we can display
 
+// This mothod builds the calendar and accounts for the changing time
 var createCalendar = function(thisMoment) {
+    // timeArray stores the times that will be built into time-blocks.  
+    // Since this is an array it would easily be adjusted to cover any time range needed
 
     var timeArray = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
-
+    //  Add current date/time to the header
     $("#currentDay").append(currentMomentString);
     var amPm = "am";
-
+    //  Using a variable to set up the time-block rows
     for (var i = 0; i < timeArray.length; i++) {
+        //translating each array value into moment objects so the time is comparable
+        // I wanted the value to be available for multiple uses
         var timeIndex = timeArray[i] + ':00' + amPm;
         var listTime = moment(timeIndex, 'h:mma').format('ha');
+        // This builds the contents of the event and time blocks of the calendar, with an editable textarea
 
         var timeEl = $("<div>")
             .addClass("time-hour-block p-5")
@@ -21,7 +30,8 @@ var createCalendar = function(thisMoment) {
         var eventEl = $("<div>")
             .addClass("event-element")
             .html("<textarea id='" + listTime + "' class='event-form' ></textarea>");
-
+        // Instead of relying on the CSS to change the background for different times, I decided to use
+        // Jquery and JavaScript to accomplish this using the colors the client wanted
 
         var compareMoment = moment(timeIndex, 'ha');
         var compareNumber = compareMoment.hour();
@@ -34,20 +44,23 @@ var createCalendar = function(thisMoment) {
         } else if (currentMomentObj.isAfter(compareMoment)) {
             eventEl.attr("style", "background-color: rgb(165, 161, 161);");
         }
+        // A button for the user to click and it will save the contents of the textarea
         var saveButton = $("<button>")
             .addClass("saveBtn btn-lg oi oi-circle-check p-5")
             .attr("id", listTime);
+        // I used the time slot as the unique id
 
-
+        // Add the elements created dynamically to the HTML
 
         $("#time-column").append(timeEl);
         $("#event-column").append(eventEl);
         $("#save-column").append(saveButton);
 
-
     }
 
 };
+// This function takes in the text and time slot to creat an event object
+// The event object is then pushed to the events array and added to the correct area within the HTML
 var addMyEvent = function(myTextEl, myTimeEl) {
     console.log(myTimeEl + " : " + myTextEl);
     var eventObj = {
@@ -57,15 +70,20 @@ var addMyEvent = function(myTextEl, myTimeEl) {
     events.push(eventObj);
     $("#" + myTimeEl).html(myTextEl);
     saveEvents();
+    // Finally the updated array is stored in localStorage for persistence
 }
-$(document).ready(function() {
 
+// Event listener for the save button element
+$(document).ready(function() {
     $("button").on("click", function() {
         var myButton = $(this);
         var timeId = myButton.attr('id');
         var myText = $.trim($("#" + timeId).val());
         if (myText.length > 0) {
+            // Make sure there is something worth saving!
             var isDuplicate = false;
+            // Check to see if there is a duplicate event entry
+            // if events is null it is not a duplicate and we can add the event to the time slot
             if (!events) {
                 addMyEvent(myText, timeId);
             } else {
@@ -75,20 +93,23 @@ $(document).ready(function() {
                         isDuplicate = true;
                     }
                 }
+                // Check array to make sure it's not a duplicate
                 if (!isDuplicate) {
                     addMyEvent(myText, timeId);
                 } else { return false; }
+                // If it is a duplicate we won't add the object and to indicate this we return falsey
             }
 
         }
     });
 
 });
-
+// Stores the object array events in localStorage using JSON stringify
 var saveEvents = function() {
     localStorage.setItem("events", JSON.stringify(events));
 };
-var loadTasks = function() {
+// Loads the object array stored in localStorage and transfers them to the events array
+var loadEvents = function() {
 
     var loadedEvents = JSON.parse(localStorage.getItem("events"));
     if (loadedEvents) {
@@ -98,10 +119,14 @@ var loadTasks = function() {
         }
         events = loadedEvents;
     } else { return false; }
+    // If there are no tasks to load then we return falsey
 
 };
+// Call function to build the calendar
 createCalendar();
-loadTasks();
+// Call function to load events stored in localStorage
+loadEvents();
+// Refreshes the page every 30 mins to keep the clock and events accurate
 setInterval(function() {
     window.location.reload();
-}, 300000);
+}, 30 * 60000);
